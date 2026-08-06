6111eeeb821a0a27c36329b078246c7e9cbc5fdb
Duda1444 <maria.e.silva66@edu.senai.br>
2026-08-06 11:10:33 -0300

    feat: ordena registros

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 //desenvolvedor 1 :
 let frutas = ["pera", "Banana", "Morango", "Laranja"];
+
 frutas.shift();
 console.table(frutas);
 
 frutas.unshift("uva");
 console.table(frutas);
 
-
 const frutaComM = frutas.find((fruta) => fruta.startsWith('M'));
 console.log(frutaComM);
+
+frutas.sort();
+console.table(frutas);
+
+
