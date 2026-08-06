d4e7d082f3dc4915bd5bb98c9d7270342faa207e
Duda1444 <maria.e.silva66@edu.senai.br>
2026-08-06 11:06:40 -0300

    feat: localiza registro

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 //desenvolvedor 1 :
-let frutas = ["maçã", "banana", "morango", "laranja"];
+let frutas = ["pera", "Banana", "Morango", "Laranja"];
 frutas.shift();
 console.table(frutas);
 
 frutas.unshift("uva");
 console.table(frutas);
+
+
+const frutaComM = frutas.find((fruta) => fruta.startsWith('M'));
+console.log(frutaComM);
