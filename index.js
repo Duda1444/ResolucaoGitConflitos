9e9f7bbb74625503ca6e15ab60f28da35c014f9a
Evelyn <evelyn.evangelista@edu.senai.br>
2026-08-06 11:25:42 -0300

    feat: remove último registro

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+//Desenvolvedor B-1
 const paises = ["Japão", "Turquia", "Noruega"];
 console.table(paises);
 
 paises.pop('Noruega');
+console.table(paises);
+
+//Desenvolvedor B-2
+paises.push();
+console.table(paises);
+
+paises.push(...["Japão", "Turquia", "Noruega", "Alemanha"]);
 console.table(paises);
